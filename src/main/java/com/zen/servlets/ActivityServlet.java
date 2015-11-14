1c513176581e613be6b4110cf662dbf38752ddac
arnaudJacquez <arnaudjacquez@mbp-de-arnaud>
2015-11-14 10:52:55 +0100

    commit du matin

---
+++ src/main/java/com/zen/servlets/ActivityServlet.java
@@ -0,0 +1,22 @@
+package com.zen.servlets;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+import com.zen.beans.Activity;
+import com.zen.dao.DAOFactory;
+//import com.zen.dao.ActivityDao;
+
+@Path("/Activities")
+@Produces({ "application/json" })
+@Consumes({ "application/json" })
+public class ActivityServlet {
+
+}
