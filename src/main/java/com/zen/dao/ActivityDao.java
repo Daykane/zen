86a3a272a9448e5a9759a166038f66262c13c476
arnaudJacquez <arnaudjacquez@mbp-de-arnaud>
2015-11-14 11:29:18 +0100

    commit du matin (2)

---
+++ src/main/java/com/zen/dao/ActivityDao.java
@@ -0,0 +1,19 @@
+package com.zen.dao;
+
+import java.util.List;
+
+import com.zen.beans.Activity;
+
+
+
+public interface ActivityDao {
+
+    void create( Activity activity ) throws DAOException;
+
+    Activity findById( String id ) throws DAOException;
+    
+    List<Activity> findAll() throws DAOException;
+    
+    void delete(int id) throws DAOException;
+    
+}
--- src/main/java/com/zen/servlets/ActivityServlet.java
+++ src/main/java/com/zen/servlets/ActivityServlet.java
@@ -12,11 +12,52 @@ import javax.ws.rs.core.UriInfo;
 
 import com.zen.beans.Activity;
 import com.zen.dao.DAOFactory;
-//import com.zen.dao.ActivityDao;
+import com.zen.dao.ActivityDao;
+
 
 @Path("/Activities")
 @Produces({ "application/json" })
 @Consumes({ "application/json" })
 public class ActivityServlet {
+
+	@Context
+	private UriInfo context;
+	private ActivityDao  activityDao;
+
+
+	@GET
+	public List<Activity> getAll() {
+		this.activityDao = DAOFactory.getInstance().getActivityDao();
+		List<Activity> activities = this.activityDao.findAll();
+		return activities;
+	}
+
+	/* OK
+	@GET
+	public void create(){
+		Activity activity = new Activity("activityName", "activityShortDesc", "activityLongDesc");
+		this.activityDao = DAOFactory.getInstance().getActivityDao();
+		this.activityDao.create(activity);
+	}
+	 */	
+	
+
+	@GET
+	@Path("{id}")
+	public Activity getOneJSON(@PathParam("id") String id) {
+		this.activityDao = DAOFactory.getInstance().getActivityDao();
+		Activity activity = this.activityDao.findById(id);
+		return activity;
+
+	}
+
+	/* ok
+	@GET
+	@Path("{id}")
+	public void delete(@PathParam("id") int id) {
+		this.activityDao = DAOFactory.getInstance().getActivityDao();
+		this.activityDao.delete(id);
+	}
+	 */
 
 }
