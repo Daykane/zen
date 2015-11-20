0e0ef2fa9332cd032b7046c8723960d9a8aba61b
Elie <elie.gallet@gmail.com>
2015-11-20 12:53:59 +0000

    fixed pagination

--- src/main/webapp/components/product-list/product-list.js
+++ src/main/webapp/components/product-list/product-list.js
@@ -28,28 +28,70 @@
         $scope.products = productFactory.query();
 
         // Public methods
-        $scope.numberOfPages = function(){
-            var products;
-            products = $scope.products;
-            products = $filter('filter')(products, $scope.searchBar);
-            products = $filter('orderBy')(products, $scope.orderProps);
-            return Math.max(Math.ceil(products.length/$scope.pageSize),1);
-        }
+        
+        $scope.prevPage = function() {
+            if ($scope.currentPage > 0) {
+                $scope.currentPage--;
+            }
+        };
+
+        $scope.setPage = function(n) {
+          $scope.currentPage=n;
+      };
+
+      $scope.prevPageDisabled = function() {
+        return $scope.currentPage === 0 ? "disabled" : "";
+    };
+
+    $scope.pageCount = function() {
+        return Math.ceil($scope.products.length/$scope.pageSize)-1;
+    };
+
+    $scope.nextPage = function() {
+        if ($scope.currentPage < $scope.pageCount()) {
+          $scope.currentPage++;
+      }
+  };
+
+  $scope.nextPageDisabled = function() {
+    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+};
         //Init
     }
 
     productListController.$inject = ['$scope', 'productListService', 'productFactory', '$filter'];
 
     angular.module('zen.components.productList', [
-            'zen.api.products',
-            'zen.services'
+        'zen.api.products',
+        'zen.services'
         ])
-        .directive('zenProductList', productList)
-        .controller('productListController', productListController)
-        .filter('startFrom', function() {
-            return function(input, start) {
+    .directive('zenProductList', productList)
+    .controller('productListController', productListController)
+    .filter('startFrom', function() {
+        return function(input, start) {
                 start = +start; //parse to int
                 return input.slice(start);
             }
-        });
+        })
+    .filter('range', function() {
+        return function(input) {
+            var lowBound, highBound;
+            switch (input.length) {
+                case 1:
+                lowBound = 0;
+                highBound = parseInt(input[0]) - 1;
+                break;
+                case 2:
+                lowBound = parseInt(input[0]);
+                highBound = parseInt(input[1]);
+                break;
+                default:
+                return input;
+            }
+            var result = [];
+            for (var i = lowBound; i <= highBound; i++)
+                result.push(i);
+            return result;
+        };
+    });
 })(window, window.angular, window._);
