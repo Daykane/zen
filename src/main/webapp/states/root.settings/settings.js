d6de7939ceb6d6af7d270e2ca0eb9f3100b01e61
JcMensonides <mensonides.jc@gmail.com>
2015-11-22 15:13:21 +0100

    r

--- src/main/webapp/states/root.settings/settings.js
+++ src/main/webapp/states/root.settings/settings.js
@@ -41,49 +41,73 @@
     	$scope.mailAlreadyUsed= false;
     	$scope.newPassword= "";
     	$scope.newPasswordConfirm="";
+    	$scope.wrongPassword=false;
     	
     	$scope.updatePassword = function(){
-    		if($scope.newPassword == $scope.newPasswordConfirm){
-    			console.log("hello");
-    			console.log($scope.newPassword);
-    			console.log($scope.newPasswordConfirm);
+    		if($scope.password!="" && $scope.newPassword!="" && $scope.newPassword == $scope.newPasswordConfirm){
+    			$scope.passwordMatch= true;
+    			data={oldPassword: $scope.oldPassword, newPassword: $scope.newPassword};
+    			$http({
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "token": authenticationService.getCookieToken
+                    },
+                    dataType: "json",
+                    url: apiUrl+'Users/'+$scope.id+'/changePassword',
+                    data: JSON.stringify(data),
+                }).then(function successCallback(response) {
+                	$state.go("root.home");
+                	$scope.wrongPassword= false;
+                  }, function errorCallback(response) {
+                	if(response.status==403 && response.data.passwordError=="true"){
+                		$scope.wrongPassword= true;
+                	}
+                  });
     		}
     		else{
     			$scope.passwordMatch= false;
-    			console.log("not hello");
     		}
     	};
     	
     	$scope.passwordMatch= true;
     	
+    	function validateEmail(email){
+    		var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+            return re.test(email);
+    	};
+    	
     	$scope.updateInfos= function(){
-    		var data= {mail: $scope.email, id: $scope.id, firstName: $scope.firstName, lastName: $scope.lastName, adr1: $scope.adr1, adr2: $scope.adr2, pc: $scope.postalCode, town: $scope.town, phone: $scope.phone};
-    		$http({
-                method: 'PUT',
-                headers: {
-                    "Content-Type": "application/json",
-                    "token": authenticationService.getCookieToken
-                },
-                dataType: "json",
-                url: apiUrl+'Users/'+$scope.id,
-                data: JSON.stringify(data),
-            }).then(function successCallback(response) {
-            	$scope.mailAlreadyUsed= false;
-            	$state.go("root.home");
-              }, function errorCallback(response) {
-            	if(response.status==400 && response.data.mailError=="true"){
-            		$scope.mailAlreadyUsed= true;
-            	}
-            	$scope.email= authenticationService.getCurrentUser().email;
-            	$scope.id= authenticationService.getCurrentUser().id;
-            	$scope.firstName= authenticationService.getCurrentUser().firstName;
-            	$scope.lastName= authenticationService.getCurrentUser().lastName;
-            	$scope.adr1= authenticationService.getCurrentUser().adr1;
-            	$scope.adr2= authenticationService.getCurrentUser().adr2;
-            	$scope.postalCode= authenticationService.getCurrentUser().postalCode;
-            	$scope.town= authenticationService.getCurrentUser().town;
-            	$scope.phone= authenticationService.getCurrentUser().phone;
-              });
+    		if(validateEmail($scope.email) && $scope.email!="" && $scope.firstName!="" && $scope.lastName!=""){
+    			var data= {mail: $scope.email, id: $scope.id, firstName: $scope.firstName, lastName: $scope.lastName, adr1: $scope.adr1, adr2: $scope.adr2, pc: $scope.postalCode, town: $scope.town, phone: $scope.phone};
+        		$http({
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "token": authenticationService.getCookieToken
+                    },
+                    dataType: "json",
+                    url: apiUrl+'Users/'+$scope.id,
+                    data: JSON.stringify(data),
+                }).then(function successCallback(response) {
+                	$scope.mailAlreadyUsed= false;
+                	$state.go("root.home");
+                  }, function errorCallback(response) {
+                	if(response.status==400 && response.data.mailError=="true"){
+                		$scope.mailAlreadyUsed= true;
+                	}
+                	$scope.email= authenticationService.getCurrentUser().email;
+                	$scope.id= authenticationService.getCurrentUser().id;
+                	$scope.firstName= authenticationService.getCurrentUser().firstName;
+                	$scope.lastName= authenticationService.getCurrentUser().lastName;
+                	$scope.adr1= authenticationService.getCurrentUser().adr1;
+                	$scope.adr2= authenticationService.getCurrentUser().adr2;
+                	$scope.postalCode= authenticationService.getCurrentUser().postalCode;
+                	$scope.town= authenticationService.getCurrentUser().town;
+                	$scope.phone= authenticationService.getCurrentUser().phone;
+                  });
+    		}
+    		
     	}
     	
     	
